bc840ff9e0aae0515724505eb4ede132916efbb6
hope2812 <71564945+hope2812@users.noreply.github.com>
2022-12-14 00:30:50 +0300

    007 Предохранители (Error Boundaries)

---
+++ src/components/errorBoundary/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import { Component } from 'react/cjs/react.production.min.js';
+import ErrorMessage from '../errorMessage/ErrorMessage.js';
+
+class ErrorBoundary extends Component {
+  state = {
+    error: false,
+  };
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: true,
+    });
+  }
+
+  render() {
+    if (this.state.error) {
+      return <ErrorMessage />;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
