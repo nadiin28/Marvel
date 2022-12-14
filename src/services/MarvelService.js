8b6870617d34ac7dece32f08735b221c9964179e
hope2812 <71564945+hope2812@users.noreply.github.com>
2022-12-14 16:35:41 +0300

    008 Пагинация данных (дозагрузка персонажей)

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=42308eec5d7abf94fc951d8af2a2450c';
+  _baseOffset = 210;
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -12,9 +13,9 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
+  getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`,
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`,
     );
     return res.data.results.map(this._transformCharacter);
   };
